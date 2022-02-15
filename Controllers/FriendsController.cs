28ccc0da6a090225fdd74f01b8d89005fcb1e7b7
Luke Allen <lukus2013@yahoo.com>
2022-02-14 20:33:30 -0600

    new

---
+++ Controllers/FriendsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpyDuh_Baratheon.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class FriendsController : ControllerBase
+    {
+    }
+}
